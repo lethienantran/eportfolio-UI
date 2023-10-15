f7b9710a1dff036e275ad8c19bbb95bf6b2fcfcb
lethienantran <lethienantran@gmail.com>
2023-10-15 03:42:51 -0700

    implement api to post project page

--- src/Pages/PostProjectPage/PostProjectPage.js
+++ src/Pages/PostProjectPage/PostProjectPage.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './PostProjectPage.css';
-
+import axios from 'axios'; 
 import StandardDropDown from '../../Components/DropDowns/StandardDropDown/StandardDropDown';
 
 import NavBar from '../../Components/NavBar/NavBar';
@@ -10,7 +10,109 @@ import StandardButton from '../../Components/Buttons/StandardButton/StandardButt
 import StandardTextInputField from '../../Components/InputFields/StandardTextInputField';
 import StandardTextAreaInputField from '../../Components/InputFields/StandardTextAreaInputField/StandardTextAreaInputField';
 
-function PostProjectPage() {
+import { connect } from 'react-redux';
+
+function PostProjectPage({ userData }) {
+  const fileInputRef = useRef(null);
+  const [imageBase64, setImageBase64] = useState(null); // State to store the base64 image data
+  const [imageOGWidth, setImageOGWidth] = useState(null);
+  const [imageOGHeight, setImageOGHeight] = useState(null);
+
+  const maxWidth = 240; 
+  const maxHeight = 240; 
+  
+  const [isError, setIsError] = useState(false);
+  
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [projectInformation, setProjectInformation] = useState ({
+    projectTitle: '',
+    collaborators: [],
+    projectDescription: '',
+  });
+
+  const requestBody = {
+    projectTitle: projectInformation.projectTitle,
+    collaborators: projectInformation.collaborators,
+    projectDescription: projectInformation.projectDescription,
+  };
+
+  const handleFileInputChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      const reader = new FileReader();
+  
+      reader.onload = (event) => {
+        const img = new Image();
+        img.src = event.target.result;
+        setImageOGHeight(img.height);
+        setImageOGWidth(img.width);
+        img.onload = () => {
+          const canvas = document.createElement('canvas');
+          let newWidth = img.width;
+          let newHeight = img.height;
+  
+          if (img.width > maxWidth || img.height > maxHeight) {
+            if (img.width / maxWidth > img.height / maxHeight) {
+              newWidth = maxWidth;
+              newHeight = (img.height / img.width) * maxWidth;
+            } else {
+              newHeight = maxHeight;
+              newWidth = (img.width / img.height) * maxHeight;
+            }
+          }
+  
+          canvas.width = newWidth;
+          canvas.height = newHeight;
+  
+          const ctx = canvas.getContext('2d');
+          ctx.drawImage(img, 0, 0, newWidth, newHeight);
+  
+          const base64Data = canvas.toDataURL('image/jpeg', 0.7); // Use 'image/jpeg' for JPEG format
+          setImageBase64(base64Data);
+        };
+      };
+  
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+  
+
+  const openFileDialog = () => {
+    fileInputRef.current.click();
+  };
+
+  const handlePostButtonClick = () => {
+    // Prepare the request body
+    const requestBody = {
+      projectTitle: projectInformation.projectTitle,
+      encodePhoto: imageBase64,
+      photoOGHeight: imageOGHeight,
+      photoOGWidth: imageOGWidth,
+      projectOwnerId: userData.userId, // Set the correct owner ID
+      projectCollaborators: projectInformation.collaborators, // Set the correct collaborators array
+      projectDescription: projectInformation.projectDescription,
+    };
+    console.log(requestBody);
+    // Make the API POST request using Axios
+    axios
+      .post('/api/project/upload', requestBody)
+      .then((response) => {
+        // Handle the response from the API, e.g., show a success message or perform further actions
+        console.log(requestBody);
+      })
+      .catch((error) => {
+        // Handle any errors that occur during the request
+        console.error('API request error:', error.response.data.message);
+      });
+  };
+
+  const HandleInputChange = (propertyName, inputValue) => {
+    if (isError) {
+      setIsError(false);
+    }
+    setProjectInformation({...projectInformation, [propertyName]: inputValue});
+  }
 
   return (
     <div className='wrapper PostProjectPage-Wrapper'>
@@ -19,18 +121,38 @@ function PostProjectPage() {
         <div className='PostProjectPage-Content'>
           <p className='heading-1 PostProjectPage-Title'>Post Your Project</p>
           <div className='PostProjectPage-UploadPhotoContainer'>
-            <IconButton icon={faImage} className='PostProjectPage-UploadPhoto'onClick={()=>{}}/>
+          <input
+              type="file"
+              accept="image/*"
+              style={{ display: 'none' }}
+              ref={fileInputRef}
+              onChange={handleFileInputChange}
+            />
+            {imageBase64 === null ? (<IconButton icon={faImage} className='PostProjectPage-UploadPhoto'onClick={openFileDialog}/>) : (
+              <div className='PostProjectPage-UploadPhotoContainer' onClick={openFileDialog}>
+                <img src={imageBase64} className='PostProjectPage-UploadedPhoto'/>
+              </div>
+            )}
+            
           </div>
           <div className='PostProjectPage-Form'>
-            <StandardTextInputField placeholder='Project Title'/>
-            <StandardDropDown placeholder='Add Collaborators' isMulti={true}/>
-            <StandardTextAreaInputField placeholder='Project Description' />
+            <StandardTextInputField placeholder='Project Title' 
+                                    onChange={HandleInputChange} 
+                                    name='projectTitle'/>
+            <StandardDropDown placeholder='Add Collaborators' isMulti={true}
+                              name='collaborators'/>
+            <StandardTextAreaInputField placeholder='Project Description' 
+                                        onChange={HandleInputChange}
+                                        name='projectDescription'/>
           </div>
-          <StandardButton title='Post' onClick={()=>{}} />
+          <StandardButton title='Post' onClick={handlePostButtonClick} />
         </div>
       </div>
     </div>
   )
 }
 
-export default PostProjectPage
+const mapStateToProps = state => ({
+  userData: state.user.userData,
+});
+export default connect(mapStateToProps)(PostProjectPage)
