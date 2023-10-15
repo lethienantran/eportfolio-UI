194d27666668ddf6c3ed11b69d615706d775029d
lethienantran <lethienantran@gmail.com>
2023-10-15 07:39:24 -0700

    finish posting project

--- src/Components/DropDowns/StandardDropDown/StandardDropDown.js
+++ src/Components/DropDowns/StandardDropDown/StandardDropDown.js
@@ -1,90 +1,140 @@
-import React from 'react'
-import Select from 'react-select';
+import React, { useEffect } from "react";
+import Select from "react-select";
 
-import './StandardDropDown.css'
-
+import "./StandardDropDown.css";
+import axios from "axios";
+import { API } from "../../../Constants";
 function StandardDropDown(props) {
-    const [selectedCollaborators, setSelectedCollaborators] = React.useState([]);
+  const [selectedCollaborators, setSelectedCollaborators] = React.useState([]);
 
-  const collaboratorsOptions = [
-    { value: 'collab1', label: 'Collaborator 1' },
-    { value: 'collab2', label: 'Collaborator 2' },
-    { value: 'collab3', label: 'Collaborator 3' },
-    { value: 'collab4', label: 'Collaborator 4' },
-    { value: 'collab5', label: 'Collaborator 5' },
-    //... add more collaborators here
-  ];
-
-  const handleCollaboratorsChange = selectedOptions => {
+  const [collaboratorsOptions, setCollaboratorsOptions] = React.useState([]);
+  const [inputValue, setInputValue] = React.useState("");
+  const handleCollaboratorsChange = (selectedOptions) => {
     setSelectedCollaborators(selectedOptions);
+    // Call the parent's function to handle collaborator changes
+    props.onCollaboratorsChange(selectedOptions);
+  };
+
+  const handleInputKeyDown = (e) => {
+    if (e.key === "Enter" && inputValue) {
+      e.preventDefault();
+      const apiURL = `/api/users/search?username=${inputValue}`;
+      axios
+        .get(apiURL, {
+          headers: {
+            "X-API-KEY": API.key,
+          },
+        })
+        .then((response) => {
+          const JSONFormat = JSON.stringify(response.data.responseObject);
+          const object = JSON.parse(JSONFormat);
+          const mappedData = object.map((item) => ({
+            value: item.userId,
+            label: item.username,
+          }));
+          setCollaboratorsOptions(mappedData);
+
+          console.log(collaboratorsOptions);
+        })
+        .catch((error) => {
+          // Handle API call error
+          console.error("API request error:", error);
+        });
+    }
+    /** Call API Search /api/users/search?username= */
   };
 
   const DropDownStyles = {
     dropdownIndicator: (provided) => ({
       ...provided,
-      display: 'none'
+      display: "none",
     }),
     indicatorSeparator: (provided) => ({
       ...provided,
-      display: 'none'
+      display: "none",
     }),
     clearIndicator: (provided) => ({
       ...provided,
-      display: 'none'
+      display: "none",
     }),
     control: (provided, state) => ({
       ...provided,
-      height: 'calc(7vmin)',
-      width: 'calc(38vmin)',
-      overflowY: 'auto',
-      borderRadius: 'calc(1vmin)',
-      border: state.isFocused ? 'solid var(--Black1) calc(0.31vmin)' : 'solid var(--Gray1) calc(0.31vmin)',
-      paddingLeft: 'calc(2.5vmin)',
-      paddingRight: 'calc(2.5vmin)',
+      height: "calc(7vmin)",
+      width: "calc(38vmin)",
+      overflowY: "auto",
+      borderRadius: "calc(1vmin)",
+      border: state.isFocused
+        ? "solid var(--Black1) calc(0.31vmin)"
+        : "solid var(--Gray1) calc(0.31vmin)",
+      paddingLeft: "calc(2.5vmin)",
+      paddingRight: "calc(2.5vmin)",
       fontFamily: '"Inter Regular"',
-      fontSize: 'calc(1.65vmin)',
-      outline: 'none',
-      boxShadow: 'none',
-      '&:hover': { borderColor: state.isFocused ? 'var(--Black1)' : 'var(--Gray1)' }
+      fontSize: "calc(1.65vmin)",
+      outline: "none",
+      boxShadow: "none",
+      "&:hover": {
+        borderColor: state.isFocused ? "var(--Black1)" : "var(--Gray1)",
+      },
     }),
     placeholder: (provided) => ({
       ...provided,
       fontFamily: '"Inter Regular"',
-      fontSize: 'calc(1.65vmin)',
+      fontSize: "calc(1.65vmin)",
     }),
     input: (provided) => ({
       ...provided,
       fontFamily: '"Inter Regular"',
-      fontSize: 'calc(1.65vmin)',
+      fontSize: "calc(1.65vmin)",
     }),
     multiValue: (provided) => ({
       ...provided,
-      overflow: 'hidden',
-      textOverflow: 'ellipsis'
+      overflow: "hidden",
+      textOverflow: "ellipsis",
     }),
     menu: (provided) => ({
       ...provided,
-      maxHeight: 'calc(12.5vmin)',
-      overflowY: 'auto'
+      maxHeight: "calc(12.5vmin)",
+      overflowY: "auto",
     }),
     multiValueRemove: (provided) => ({
       ...provided,
-      fontSize: 'calc(1.65vmin)',
+      fontSize: "calc(1.65vmin)",
     }),
   };
 
+  useEffect(() => {
+    setInputValue("");
+  }, [selectedCollaborators]);
+
   return (
-    <Select 
-    isMulti={props.isMulti}
-    name={props.name}
-    value={selectedCollaborators} //sample
-    onChange={handleCollaboratorsChange} //sample
-    options={collaboratorsOptions} //sample
-    placeholder={props.placeholder}
-    styles={DropDownStyles}
-    className={`${props.className} StandardDropDown`}
-    />
-  )
+    <>
+      {props.isCollaboratorDropdown ? (
+        <Select
+          isMulti={props.isMulti}
+          name={props.name}
+          value={selectedCollaborators}
+          onChange={handleCollaboratorsChange}
+          options={collaboratorsOptions}
+          placeholder={props.placeholder}
+          styles={DropDownStyles}
+          className={`${props.className} StandardDropDown`}
+          onInputChange={(value) => setInputValue(value)}
+          onKeyDown={handleInputKeyDown}
+        />
+      ) : (
+        <Select
+          isMulti={props.isMulti}
+          name={props.name}
+          value={selectedCollaborators}
+          onChange={handleCollaboratorsChange}
+          options={collaboratorsOptions}
+          placeholder={props.placeholder}
+          styles={DropDownStyles}
+          className={`${props.className} StandardDropDown`}
+        />
+      )}
+    </>
+  );
 }
 
-export default StandardDropDown
+export default StandardDropDown;
