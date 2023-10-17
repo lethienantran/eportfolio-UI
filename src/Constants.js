61841ba6d2c10650b777e8becbb8e704c860f4e5
Le Thien An Tran <114910291+lethienantran@users.noreply.github.com>
2023-10-16 18:32:45 -0700

    change API URL

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 export const API = {
     key: 'JZD1zC0efO7rnomTaNr8f1aoL6WRup5z4BmOEWut',
-    signInURL: '/api/authentication/sign-in',
+    signInURL: 'https://bvyhgijzjg.execute-api.us-west-2.amazonaws.com/api/authentication/sign-in',
     signUpURL: 'https://bvyhgijzjg.execute-api.us-west-2.amazonaws.com/EP_API_PROD/api/authentication/sign-up',
-    editUserInformationURL: '/api/users/information',
-    followURL: '/api/follow/new',
+    editUserInformationURL: 'https://bvyhgijzjg.execute-api.us-west-2.amazonaws.com/api/users/information',
+    followURL: 'https://bvyhgijzjg.execute-api.us-west-2.amazonaws.com/api/follow/new',
 }
