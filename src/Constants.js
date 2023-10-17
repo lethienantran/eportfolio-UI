59f0eb5c640bd36078a3359771bf02fe95a40907
Le Thien An Tran <114910291+lethienantran@users.noreply.github.com>
2023-10-16 17:14:28 -0700

    test api URL

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 export const API = {
     key: 'JZD1zC0efO7rnomTaNr8f1aoL6WRup5z4BmOEWut',
     signInURL: '/api/authentication/sign-in',
-    signUpURL: '/api/authentication/sign-up',
+    signUpURL: 'https://bvyhgijzjg.execute-api.us-west-2.amazonaws.com/EP_API_PROD/api/authentication/sign-up',
     editUserInformationURL: '/api/users/information',
     followURL: '/api/follow/new',
 }
